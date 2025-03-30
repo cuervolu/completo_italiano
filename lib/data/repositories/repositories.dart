375fcc6f10f1d975f22c707758c723022d726e1e
Cuervolu <angel.cuervo187@gmail.com>
2025-03-30 13:15:01 -0300

    feat: add repositories index

---
+++ lib/data/repositories/repositories.dart
@@ -0,0 +1,5 @@
+export 'character_repository.dart';
+export 'development_repository.dart';
+export 'relationships_repository.dart';
+export 'settings_repository.dart';
+export 'story_repository.dart';
